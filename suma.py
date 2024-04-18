291d986a2e4d3254daabe3f99022caf07268072f
rociolr96 <rociolr96vi@gmail.com>
2024-04-18 15:49:35 +0200

    e

--- suma.py
+++ suma.py
@@ -3,3 +3,27 @@ def sumar(a, b):
 
 if __name__=="__main__":
     print(sumar(5,3))
+
+def restar(a, b):
+    return a - b
+
+if __name__=="__main__":
+    print(restar(2,-1))    
+
+def multiplicar(a, b):
+    return a * b
+
+if __name__=="__main__":
+    print(multiplicar(5,3))    
+
+
+def dividir(a, b):
+   # return a / b
+ if b==0:
+     return {"message":"error no se puede dividir por 0"}
+ else:
+      resultado=a/b
+      return {"resultado": f"el resultado es {resultado}"}
+              
+if __name__=="__main__":
+    print(dividir(5,3))    
--- test_suma.py
+++ test_suma.py
@@ -1,11 +1,38 @@
 import unittest
 from suma import sumar
+from suma import restar
+from suma import multiplicar
+from suma import dividir
+
+
 class TestSumar(unittest.TestCase):
         
         def test_sumar(self):
           self.assertEqual(sumar(3,2), 5)
           self.assertEqual(sumar(-1,1), 0)
           self.assertEqual(sumar(-1,-1), -2)
+          self.assertEqual(sumar(-1,0), -1)
+        def test_restar(self):
+          self.assertEqual(restar(2,1), 1)
+          self.assertEqual(restar(2,-1), 3)
+          self.assertEqual(restar(-2,-1), -1)
+          self.assertEqual(restar(2,0), 2)
+        def test_multiplicar(self):  
+          self.assertEqual(multiplicar(2,1), 2)
+          self.assertEqual(multiplicar(2,-1), -2)
+          self.assertEqual(multiplicar(-2,-1), 2)
+          self.assertEqual(multiplicar(-2,0), 0)
+
+        def test_dividir(self):  
+          self.assertEqual(dividir(2,1), {'resultado': 'el resultado es 2.0'} )
+          self.assertEqual(dividir(2,-1), {'resultado': 'el resultado es -2.0'})
+          self.assertEqual(dividir(-2,-1), {'resultado': 'el resultado es 2.0'})
+
+
+          self.assertEqual(dividir(2,0),{"message":"error no se puede dividir por 0"} )
+
+
+
 if __name__=='__main__':
       unittest.main()
     
